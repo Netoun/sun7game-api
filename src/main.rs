99b8ab61e523fd5201f2e1b0b61c81bbd9f639dd
Netoun <coulonnier.nicolas.44@gmail.com>
2018-06-07 16:25:10 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ extern crate serde_derive;
 extern crate bson;
 extern crate mongodb;
 
-use rocket::http::Method;
-use rocket::response::Responder;
-use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, Guard};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{ContentType, Header, Method};
+use rocket::{Request, Response};
+use std::io::Cursor;
 
 use mongodb::db::ThreadedDatabase;
 use mongodb::{Client, CommandType, ThreadedClient};
@@ -27,19 +28,51 @@ pub struct Score {
     pub point: u32,
 }
 
+pub struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response,
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON)
+        {
+            response.set_header(Header::new(
+                "Access-Control-Allow-Origin",
+                "http://localhost:9000",
+            ));
+            response.set_header(Header::new(
+                "Access-Control-Allow-Methods",
+                "POST, GET, OPTIONS",
+            ));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+
+        if request.method() == Method::Options {
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(Cursor::new(""));
+        }
+    }
+}
+
 fn connect_db() -> mongodb::coll::Collection {
     let client = Client::connect(
         &env::var("MONGO_URL").unwrap(),
         env::var("MONGO_PORT").unwrap().parse::<u16>().unwrap(),
     ).unwrap();
     let db = client.db(&env::var("USER").unwrap());
-
+    db.auth(&env::var("USER").unwrap(), &env::var("PASSWORD").unwrap())
+        .unwrap();
     return db.collection("score");
 }
 
 #[post("/", format = "application/json", data = "<score>")]
-fn record_score(cors: Guard, score: Json<Score>) -> rocket_cors::Responder<&str> {
-    let options = cors_options_all();
+fn record_score(score: Json<Score>) -> String {
     let coll = connect_db();
     let name = &score.name;
     let point = score.point;
@@ -48,48 +81,23 @@ fn record_score(cors: Guard, score: Json<Score>) -> rocket_cors::Responder<&str>
         "point": point
     };
     match coll.insert_one(doc, None) {
-        Ok(_) => cors.responder("Done"),
-        Err(e) => cors.responder("e"),
+        Ok(_) => "Done".to_string(),
+        Err(e) => e.to_string(),
     }
 }
 
 #[get("/", format = "application/json")]
-fn get_scores<'a>() -> Json<Value> {
+fn get_scores() -> Json<Value> {
     let coll = connect_db();
     let mut cursor = coll.find(None, None).ok().expect("Failed to execute find.");
     let docs: Vec<_> = cursor.map(|doc| doc.unwrap()).collect();
     return Json(json!(docs));
 }
 
-#[options("/score")]
-fn get_scores_options<'r>() -> impl Responder<'r> {
-    let options = cors_options_all();
-    options.respond_owned(|guard| guard.responder(()))
-}
-
-fn cors_options() -> Cors {
-    let (allowed_origins, failed_origins) = AllowedOrigins::some(&["http://sun7game.netlify.com/"]);
-
-    // You can also deserialize this
-    rocket_cors::Cors {
-        allowed_origins: allowed_origins,
-        ..Default::default()
-    }
-}
-
-fn cors_options_all() -> Cors {
-    // You can also deserialize this
-    Default::default()
-}
-
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount(
-            "/score",
-            routes![get_scores, record_score, get_scores_options],
-        )
-        .mount("/score", rocket_cors::catch_all_options_routes())
-        .manage(cors_options())
+        .attach(CORS())
+        .mount("/score", routes![get_scores, record_score])
 }
 
 fn main() {
