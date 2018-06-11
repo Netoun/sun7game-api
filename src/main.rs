fc8403c5f6bbc238835354950afb117e2bf20e41
Netoun <coulonnier.nicolas.44@gmail.com>
2018-06-11 15:03:35 +0200

    add logo

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,10 @@ fn get_scores() -> Json<Value> {
 pub fn options() -> rocket_cors::Cors {
     rocket_cors::Cors {
         allowed_origins: AllowedOrigins::all(),
-        allowed_methods: vec![Method::Post].into_iter().map(From::from).collect(),
+        allowed_methods: vec![Method::Post, Method::GET]
+            .into_iter()
+            .map(From::from)
+            .collect(),
         allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Content-Type"]),
         allow_credentials: true,
         ..Default::default()
