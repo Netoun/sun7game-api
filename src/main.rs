a72886304b9fa68c1b302ab7a0fd87f990f842bd
Netoun <coulonnier.nicolas.44@gmail.com>
2018-06-12 10:29:34 +0200

    fix push

--- src/main.rs
+++ src/main.rs
@@ -63,10 +63,7 @@ fn get_scores() -> Json<Value> {
 pub fn options() -> rocket_cors::Cors {
     rocket_cors::Cors {
         allowed_origins: AllowedOrigins::all(),
-        allowed_methods: vec![Method::Post, Method::Get]
-            .into_iter()
-            .map(From::from)
-            .collect(),
+        allowed_methods: vec![Method::Post].into_iter().map(From::from).collect(),
         allowed_headers: AllowedHeaders::some(&["Authorization", "Accept", "Content-Type"]),
         allow_credentials: true,
         ..Default::default()
